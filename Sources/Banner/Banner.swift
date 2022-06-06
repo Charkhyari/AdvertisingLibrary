5d3141ce6f860e8ed749333e9732b42cc1464bfb
Charkhyari <103569684+Charkhyari@users.noreply.github.com>
2022-06-06 22:14:51 +0100

    Add files via upload
    Updated method.

--- Sources/Banner/Banner.swift
+++ Sources/Banner/Banner.swift
@@ -1,10 +1,22 @@
+import SwiftUI
+
 public struct Banner {
     public private(set) var text = "Hello, World!"
-
+    
     public init() {
     }
     
-    public func sayHello() -> String {
-        return "Hello"
+    @available(iOS 13.0, *)
+    public func addAdBanner() {
+        @ViewBuilder
+        func fetchView() -> some View {
+            if #available(iOS 15.0, *) {
+                ContentView()
+            } else {
+                // Fallback on earlier versions
+            }
+        }
     }
 }
+
+
